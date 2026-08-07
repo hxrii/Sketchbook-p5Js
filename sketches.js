a5eda462072042c55b2b78943a24d769c6b3282c
hari <harikrishnankannoth@gmail.com>
2026-08-07 13:39:34 +0530

    Bubble Trail sketch added

--- sketches.js
+++ sketches.js
@@ -5,13 +5,23 @@ document.getElementById("last-updated").textContent = lastUpdated;
 const SKETCHES = [
   
   {
-  title: "Bubble World 1",
-  file: "recursive-tree.js",
+  title: "Bubble World",
+  file: "bubble-world.js",
   description: "A tiny world of wandering bubbles with random colors. Pop them with a click and draw new ones as you drag.",
   link: "https://editor.p5js.org/hxrii/full/vV4-fwCWV",
-  tag: "INTERACTIVE",
+  tag: "DESKTOP INTERACTIVE",
   dims: "600×600",
   embed: "https://editor.p5js.org/hxrii/embed/vV4-fwCWV",
+  thumb: ""
+  },
+  {
+  title: "Bubble Trail",
+  file: "bubble-trail.js",
+  description: "You're leaving a trail... luckily it's only bubbles. The FBI can relax. Click and drag the mouse",
+  link: "https://editor.p5js.org/hxrii/full/bYtSA9fXs",
+  tag: "DESKTOP INTERACTIVE",
+  dims: "600×600",
+  embed: "https://editor.p5js.org/hxrii/embed/bYtSA9fXs",
   thumb: ""
   },
 ];
