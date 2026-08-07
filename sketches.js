eb6ee9b407f7bc11ad9bf63b76bebd8363c088bb
hari <harikrishnankannoth@gmail.com>
2026-08-07 12:18:54 +0530

    First Commit

---
+++ sketches.js
@@ -0,0 +1,20 @@
+const lastUpdated = "AUG 7, 2026";
+document.getElementById("last-updated").textContent = lastUpdated;
+
+
+const SKETCHES = [
+  
+  {
+  title: "Bubble World 1",
+  file: "recursive-tree.js",
+  description: "A tiny world of wandering bubbles with random colors. Pop them with a click and draw new ones as you drag.",
+  link: "https://editor.p5js.org/hxrii/full/vV4-fwCWV",
+  tag: "INTERACTIVE",
+  dims: "600×600",
+  embed: "https://editor.p5js.org/hxrii/embed/vV4-fwCWV",
+  thumb: ""
+  },
+];
+
+
+
